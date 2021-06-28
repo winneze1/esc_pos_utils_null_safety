fc397fefc024fef7934d13a43d01a5981c2a71f1
LUCAS ALVES <lucas.ra.mast@gmail.com>
2021-06-28 15:35:06 -0300

    v1.0.0

--- lib/esc_pos_utils.dart
+++ lib/esc_pos_utils.dart
@@ -9,7 +9,9 @@
 library esc_pos_utils;
 
 export './src/barcode.dart';
+export './src/capability_profile.dart';
 export './src/enums.dart';
 export './src/pos_column.dart';
 export './src/pos_styles.dart';
-export './src/ticket.dart';
+export './src/qrcode.dart';
+export './src/generator.dart';
